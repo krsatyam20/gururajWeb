acb27229f16ad84ebac9717b32f59a341c866fd0
satyam <39213202+krsatyam20@users.noreply.github.com>
2019-08-18 15:03:41 +0530

    Add files via upload

---
+++ phpClass.php
@@ -0,0 +1,69 @@
+<?php 
+
+// print any things 
+echo "GuruRaj"; 
+//decelear variable and assign the value 
+
+$VariableName=10;
+
+$x=10;
+
+echo $x;
+
+$x="GuruRaj";
+
+echo $x;
+
+$x=40;
+$y=20;
+
+$z=$x+$y;
+
+echo $z;
+
+if($x>$y){
+	
+	echo "X is GT";
+}
+else {
+	echo "Y is GT";
+	
+}
+// nested if else 
+	
+
+$x=10;
+$y=30;
+$z=40;
+
+if($x>$y){
+	if($x>$z){ 
+		echo "X is GT";
+	}
+	else {
+		
+		echo "Z is GT";
+	}
+}
+else {
+	
+	if($y>$z){ 
+		echo "y is GT";
+	}
+	else {
+		
+		echo "Z is GT";
+	}
+}
+
+
+
+
+
+
+// pre pending class 
+// switch loop function 
+
+
+
+?>
